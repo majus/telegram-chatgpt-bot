ba39afb169c012f6c81165fc4ba48cc38b56e09e
Denis Perov <denis.perov@gmail.com>
2024-03-09 15:54:29 +0300

    Support OpenAI modal customization via environment variables

--- src/openai.js
+++ src/openai.js
@@ -1,9 +1,9 @@
-import { Configuration, OpenAIApi } from "openai";
-import { createReadStream, createWriteStream } from "fs";
-import axios from "axios";
-import dotenv from "dotenv";
-import { dirname, resolve } from "path";
-import { fileURLToPath } from "url";
+import { Configuration, OpenAIApi } from 'openai';
+import { createReadStream, createWriteStream } from 'fs';
+import axios from 'axios';
+import dotenv from 'dotenv';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
@@ -11,9 +11,9 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 class OpenAI {
   roles = {
-    ASSISTANT: "assistant",
-    USER: "user",
-    SYSTEM: "system",
+    ASSISTANT: 'assistant',
+    USER: 'user',
+    SYSTEM: 'system',
   };
 
   constructor() {
@@ -26,51 +26,51 @@ class OpenAI {
   async chat(messages) {
     try {
       const response = await this.openAi.createChatCompletion({
-        model: "gpt-3.5-turbo",
+        model: process.env.OPENAI_MODEL,
         messages,
       });
 
       return response.data.choices[0].message;
     } catch (e) {
-      console.log("Error while chat", e.message);
+      console.log('Error while chat', e.message);
     }
   }
 
   async translate(messages) {
     try {
       const response = await this.openAi.createChatCompletion({
-        model: "gpt-3.5-turbo",
+        model: process.env.OPENAI_MODEL,
         messages,
       });
 
       return response.data.choices[0].message;
     } catch (e) {
-      console.log("Error while chat", e.message);
+      console.log('Error while chat', e.message);
     }
   }
 
   async imageGeneration(description, fileName) {
     try {
-      const imagePath = resolve(__dirname, "../images", `${fileName}.jpeg`);
+      const imagePath = resolve(__dirname, '../images', `${fileName}.jpeg`);
       const response = await this.openAi.createImage({
         prompt: description,
-        size: "1024x1024",
+        size: '1024x1024',
         n: 1,
       });
       const axiosResponse = await axios({
-        method: "get",
+        method: 'get',
         url: response.data.data[0].url,
-        responseType: "stream",
+        responseType: 'stream',
       });
 
       return new Promise((resolve, reject) => {
         const stream = createWriteStream(imagePath);
         axiosResponse.data.pipe(stream);
-        stream.on("finish", () => resolve({ path: response.data.data[0].url }));
-        stream.on("error", (error) => reject(error.message));
+        stream.on('finish', () => resolve({ path: response.data.data[0].url }));
+        stream.on('error', (error) => reject(error.message));
       });
     } catch (e) {
-      console.log("Error while imageGeneration", e.message);
+      console.log('Error while imageGeneration', e.message);
     }
   }
 
@@ -79,11 +79,11 @@ class OpenAI {
       const stream = createReadStream(filePath);
       const response = await this.openAi.createTranscription(
         stream,
-        "whisper-1"
+        'whisper-1'
       );
       return response.data.text;
     } catch (e) {
-      console.log("Error while file transcription", e.message);
+      console.log('Error while file transcription', e.message);
     }
   }
 }
